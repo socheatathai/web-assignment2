8ca17e24b4d6cd01901a8aeb9e24e50af90db597
socheatathai <socheatathai62@gmail.com>
2024-03-18 10:06:35 +0700

    update to date

--- backend/api/config/conection-example.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "name";
-    $password = "yourPassword";
-    $dbname = "YourDBName";
-    $conn = new mysqli($server, $username, $password, $dbname);
-?>
--- backend/api/config/conection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "web1";
-$conn = new mysqli($server, $username, $password, $dbname);
-?>
